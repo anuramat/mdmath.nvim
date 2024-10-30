66435ac3d2a2f7695b73223f87b400b43e752fbc
Thiago Mota Martins <thiagomota510@gmail.com>
2024-10-30 19:00:02 -0300

    added docs to configuration

--- lua/mdmath/config.lua
+++ lua/mdmath/config.lua
@@ -1,8 +1,15 @@
 local default_opts = {
+    -- Filetypes that the plugin will be enabled by default.
     filetypes = {'markdown'},
-    foreground = 'Normal',
+    -- Color of the equation, can be a highlight group or a hex color.
+    -- Examples: 'Normal', '#ff0000'
+    foreground = 'Normal', 
+    -- Hide the text when the equation is under the cursor.
     anticonceal = true,
+    -- Hide the text when in the Insert Mode.
     hide_on_insert = true,
+    -- Scale of the equation images, increase to prevent blurry images when increasing terminal
+    -- font, high values may produce aliased images.
     scale = 1.0,
 }
 
